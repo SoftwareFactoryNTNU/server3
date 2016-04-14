13bef11429abf92af7355570f61c104dff99281b
mariusmoe <mariuom@stud.ntnu.no>
2016-04-14 17:20:07 +0200

    wooha test is working like a champ!

--- server/test.js
+++ server/test.js
@@ -17,4 +17,19 @@ describe('loading express', function () {
       .get('/foo/bar')
       .expect(404, done);
   });
+  it('login without login', function testPath(done) {
+    request(server)
+      .put('/api/me')
+      .expect(401, done);
+  });
+  it('logged in', function(done) {
+    request(server)
+      .post('/auth/login_admin')
+      .send({
+        "email": "marius_oscar@live.no",
+        "password": "aristoteles"
+      })
+      .expect(200,done)
+  });
+
 });
